e79bbad72e094d2c55c340bd27af807bcd8e8f08
Vlad <vladmdgolam@gmail.com>
2025-10-05 17:52:23 +0200

    Revert "feat: v0"
    This reverts commit 0ff294c61670ebca8e60dcb445c670d13f79d4f8.

--- app/api/capitals/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { capitals } from "@/data/capitals";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  return NextResponse.json({ capitals });
-}
